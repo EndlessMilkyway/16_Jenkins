bb54145ae867f9b417997fc686f71ab69c1231ee
EndlessMilkyway <freekyr7529@gmail.com>
2025-06-02 11:35:45 +0900

    edit controller

--- src/test/java/com/endlessmilkyway/jenkins/controller/CalculatorController.java
+++ src/test/java/com/endlessmilkyway/jenkins/controller/CalculatorController.java
@@ -2,12 +2,10 @@ package com.endlessmilkyway.jenkins.controller;
 
 import com.endlessmilkyway.jenkins.dto.CalculatorDTO;
 import com.endlessmilkyway.jenkins.service.CalculatorService;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@Slf4j
 @RestController
 public class CalculatorController {
 
@@ -26,7 +24,7 @@ public class CalculatorController {
     @GetMapping("/plus")
     public ResponseEntity<CalculatorDTO> plusTowNumbers(CalculatorDTO calculatorDTO) {
 
-        log.info("plus 핸들러에 전달되는 값 확인 : {}", calculatorDTO);
+//        log.info("plus 핸들러에 전달되는 값 확인 : {}", calculatorDTO);
 
         int result = calculatorService.plusTwoNumbers(calculatorDTO);
 
